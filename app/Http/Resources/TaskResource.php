70e340a025e4cb9a5ce159b794bfe0da742e507e
abdulaziz <abdulazizredi90@gmail.com>
2025-01-24 22:20:51 +0300

    task rendering

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -4,6 +4,10 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\ProjectResource;
+use Carbon\Carbon;
+
 
 class TaskResource extends JsonResource
 {
@@ -14,6 +18,20 @@ class TaskResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
+            'updated_at' => (new Carbon($this->updated_at))->format('Y-m-d'),
+            'due_date' => (new Carbon($this->due_date))->format('Y-m-d'),
+            'status' => $this->status,
+            'priority' => $this->priority,
+            'project' => new ProjectResource($this->project),
+            'assinedTo' =>$this->assinedTo? new UserResource($this->assinedTo):null,
+            'createdBy' => new UserResource($this->createdBy),
+            'updatedBy' => new UserResource($this->updatedBy),
+            'image_path' => $this->image_path,
+        ];
     }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,8 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function assignedTo()
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+
 }
