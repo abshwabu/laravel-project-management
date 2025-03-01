86aca8c5a4254e914fdbf13398d832e6af6fcc1e
abdulaziz <abdulazizredi90@gmail.com>
2025-03-01 11:01:09 +0300

    success message

--- resources/js/Pages/Task/Index.jsx
+++ resources/js/Pages/Task/Index.jsx
@@ -3,7 +3,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link } from "@inertiajs/react";
 import TasksTable from "./TasksTable";
 
-export default function Index({auth, tasks, queryParams = null}) {
+export default function Index({auth, tasks, queryParams = null, success}) {
 
     
     
@@ -27,7 +27,7 @@ export default function Index({auth, tasks, queryParams = null}) {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <TasksTable tasks={tasks} queryParams={queryParams} />
+                            <TasksTable tasks={tasks} queryParams={queryParams} success={success}/>
                         </div>
                     </div>
                 </div>
